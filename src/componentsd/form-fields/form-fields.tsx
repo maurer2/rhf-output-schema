ea5c13bfe393dc3acb4eca5cbf05b262da3f2655
Martin <mr.mmaurer@gmail.com>
2024-10-26 23:27:25 +0200

    Added inert via undocumented disabled attribute

--- src/componentsd/form-fields/form-fields.tsx
+++ src/componentsd/form-fields/form-fields.tsx
@@ -1,17 +1,17 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { ErrorMessage } from "@hookform/error-message";
-import { DevTool } from "@hookform/devtools";
-import { useState } from "react";
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ErrorMessage } from '@hookform/error-message';
+import { DevTool } from '@hookform/devtools';
+import { useState, useReducer } from 'react';
 
 export const formSchema = z.object({
   name: z
     .string()
     .min(1)
-    .transform((value) => value.split(" ")),
+    .transform((value) => value.split(' ')),
   age: z.string().min(1).pipe(z.coerce.number()),
-  isActive: z.boolean().transform((value) => (value ? "Yay" : "Nay")),
+  isActive: z.boolean().transform((value) => (value ? 'Yay' : 'Nay')),
 });
 
 type FormSchema = z.input<typeof formSchema>;
@@ -19,17 +19,21 @@ type FormSchemaTransformed = z.output<typeof formSchema>;
 
 export default function FormFields() {
   const [transformedValues, setTransformedValues] = useState<FormSchemaTransformed | null>(null);
+  const [isDisabled, toggleIsDisabled] = useReducer((state: boolean) => !state, false);
+
+  useState<FormSchemaTransformed | null>(null);
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, disabled },
     control,
   } = useForm<FormSchema, undefined, FormSchemaTransformed>({
     defaultValues: {
-      name: "Name1 Name2",
-      age: "50",
+      name: 'Name1 Name2',
+      age: '50',
       isActive: true,
     },
+    disabled: isDisabled,
     resolver: zodResolver(formSchema),
   }); // https://github.com/react-hook-form/react-hook-form/issues/12050
 
@@ -46,21 +50,23 @@ export default function FormFields() {
         name="test form"
         aria-label="test form"
         onSubmit={handleSubmit(onSubmit)}
+        // @ts-ignore
+        inert={disabled ? 'inert' : ''}
       >
         <div className="field">
           <label htmlFor="name">Name</label>
-          <input {...register("name")} id="name" />
+          <input {...register('name')} id="name" />
           <ErrorMessage errors={errors} name="name" />
         </div>
 
         <div className="field">
           <label htmlFor="age">Age</label>
-          <input {...register("age")} id="age" />
+          <input {...register('age')} id="age" />
           <ErrorMessage errors={errors} name="age" />
         </div>
 
         <div className="field-checkbox">
-          <input type="checkbox" {...register("isActive")} id="isActive" />
+          <input type="checkbox" {...register('isActive')} id="isActive" />
           <label htmlFor="isActive">is active</label>
           <div className="error">
             <ErrorMessage errors={errors} name="isActive" />
@@ -73,6 +79,9 @@ export default function FormFields() {
           {transformedValues ? <output>{JSON.stringify(transformedValues, null, 4)}</output> : null}
         </div>
       </form>
+      <button type="button" onClick={toggleIsDisabled}>
+        Toggle form inert status
+      </button>
       <DevTool control={control} />
     </>
   );
